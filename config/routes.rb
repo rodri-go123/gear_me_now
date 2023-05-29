93a6824ff7e882fd6706895d50f3ab05c82c4609
Rodrigo Cardoso <rodrigo.garrett.cardoso@gmail.com>
2023-05-29 15:52:48 +0200

    initialised User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
+  # change is to items#index later!
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
