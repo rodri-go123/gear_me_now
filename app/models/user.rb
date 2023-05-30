e2017fb71c55a710beb24c3fb8350ff563c2f335
Amanda Sefu Beierl <atsefu@gmail.com>
2023-05-30 15:16:29 +0200

    fixed signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, :last_name, presence: true
   has_many :items
   has_many :rentals
 end
