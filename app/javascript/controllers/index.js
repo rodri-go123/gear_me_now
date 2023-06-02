333128fff43adbbe312497a2b4868df072a42da8
Rodrigo Cardoso <rodrigo.garrett.cardoso@gmail.com>
2023-06-02 12:57:09 +0200

    Added map styling in show page

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("hello", HelloController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
+
+import ShowController from "./show_controller"
+application.register("show", ShowController)
